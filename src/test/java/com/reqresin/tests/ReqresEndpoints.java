f4c11eb86d807741b2d9ea8f8b8cc99a2112b665
Anton Vlasov <102616533+tonn1oo@users.noreply.github.com>
2022-06-10 15:22:03 +0300

    Update ReqresEndpoints.java

--- src/test/java/com/reqresin/tests/ReqresEndpoints.java
+++ src/test/java/com/reqresin/tests/ReqresEndpoints.java
@@ -7,9 +7,9 @@ public class ReqresEndpoints {
     public static final String listUsers = "users/2";
     public static final String notFoundUsers = "users/23";
     public static final String createUser = "users";
-    public static final String  register = "register";
-    public static final String  delete = "users/2";
-    public static final String  login = "login";
+    public static final String register = "register";
+    public static final String delete = "users/2";
+    public static final String login = "login";
 
 
 
